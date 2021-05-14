d87042fd53a71e41c94ed514eadce7343ade956d
gretaivan <greta.ivan91@gmail.com>
2021-05-14 16:01:01 +0100

    creates public app and adds config with / route

--- mediquick/mediquick/settings.py
+++ mediquick/mediquick/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
