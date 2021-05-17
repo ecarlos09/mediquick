eb4301a5aec759b3a66f67d4ee1cb9e8b2b30c88
gretaivan <greta.ivan91@gmail.com>
2021-05-17 11:45:58 +0100

    adds login render

--- public/views.py
+++ public/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'public/home.html')
+
+    
+def login(request):
+    
+    return render(request, 'public/login.html', title="login")
