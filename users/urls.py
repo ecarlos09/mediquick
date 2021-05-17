d1c9fd1ded0f499b5ae1e283f971d4a71e3231a9
gretaivan <greta.ivan91@gmail.com>
2021-05-17 18:36:03 +0100

    fixes register route

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.auth_view, name='login'),
     # path('register', views.index, name='index'),
-    path('register', views.register, name='register'),
+    path('register/', views.register, name='register'),
 ]
