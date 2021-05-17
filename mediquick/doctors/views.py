00f06f611d05acb5caa54cdfa00869cc0a1f3c2b
Elwin <elwin.carlos09@gmail.com>
2021-05-17 15:51:02 +0100

    add permissions decorator

--- mediquick/doctors/views.py
+++ mediquick/doctors/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from guardian.decorators import permission_required_or_403
 
 # Create your views here.
 
@@ -11,6 +12,8 @@ from django.contrib.auth.decorators import login_required
 # def server_error_500(request):
 #     return render(request, '500.html')
 
+@permission_required_or_403('auth.change_group',
+(Group, 'name', 'group_name'))
 def doctor_home(request):
     title = "Your Profile"
     name = "Strange"
