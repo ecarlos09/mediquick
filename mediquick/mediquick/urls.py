eb0b4e150ee36500610e21e9df798c96b1e14664
Elwin <elwin.carlos09@gmail.com>
2021-05-17 16:20:42 +0100

    add delimiter to url patterns!

--- mediquick/mediquick/urls.py
+++ mediquick/mediquick/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('public.urls')),
     path('admin/', admin.site.urls),
-    path('doctors/', include('doctors.urls'))
+    path('doctors/', include('doctors.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
