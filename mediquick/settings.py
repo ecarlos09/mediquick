b139d3262356f1e9979a7483c62ce774bb7c9add
gretaivan <greta.ivan91@gmail.com>
2021-05-17 11:47:06 +0100

    merged with users

--- mediquick/settings.py
+++ mediquick/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'public'
+    'public',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'user-home'
+LOGIN_URL = 'login'
