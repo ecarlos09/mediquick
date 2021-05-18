76fe893c341a7743c36948f03f7be27e75aa9de2
Elwin <elwin.carlos09@gmail.com>
2021-05-18 16:58:00 +0100

    remove old url patterns

--- mediquick/urls.py
+++ mediquick/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', include('public.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
-    
+    path('doctors/', include('doctors.urls')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),    
     # path('register/', user_views.register, name='register'),
     # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     # path('login/', auth_view, name='login-view'),
@@ -32,8 +33,8 @@ urlpatterns = [
     # two factor
   
 # two factor end 
-    path('doctors/', include('doctors.urls')),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
+    
+    # path('register/', user_views.register, name='register'),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    
 ]
