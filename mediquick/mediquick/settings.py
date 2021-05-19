cf7e7f50ef7bb9e6deece35cc07b23ad94fa1f68
Elwin <elwin.carlos09@gmail.com>
2021-05-19 10:49:00 +0100

    edit static file collection

--- mediquick/mediquick/settings.py
+++ mediquick/mediquick/settings.py
@@ -147,9 +147,11 @@ EMAIL_PORT = 25
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
 
