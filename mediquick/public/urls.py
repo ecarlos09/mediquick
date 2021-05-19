b36beb145ece819581b1222205cb47529a476702
Elwin <elwin.carlos09@gmail.com>
2021-05-19 20:16:37 +0100

    add url pattern for forbidden access

--- mediquick/public/urls.py
+++ mediquick/public/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('405-fobidden/', views.forbidden_405, name='405')
 ]
--- mediquick/public/views.py
+++ mediquick/public/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'public/home.html')
 
+def forbidden_405(request):
+    return render(request, 'public/errors/405.html')
+
     
 # def login(request):
 #     return render(request, 'public/login.html', title="login")
