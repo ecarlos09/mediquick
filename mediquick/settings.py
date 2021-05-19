b8cdd5c014ab007fc21b39b0d4d2ecedc3323a47
Elwin <elwin.carlos09@gmail.com>
2021-05-19 11:30:42 +0100

    change static files referencing

--- mediquick/settings.py
+++ mediquick/settings.py
@@ -161,15 +161,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 ''' Deployment configuration '''
 
 django_heroku.settings(locals())
-
-
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
@@ -178,3 +173,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
