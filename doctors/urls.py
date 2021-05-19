4c7bfdcf5ff3723e126a686496ef6c5bf1f10a18
Elwin <elwin.carlos09@gmail.com>
2021-05-19 08:15:35 +0100

    change doctor home page url

--- doctors/urls.py
+++ doctors/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.doctor_home, name='doctor-home'),
+    path('home/<user_id>', views.doctor_home, name='doctor-home'),
     path('schedule/', views.doctor_schedule, name='doctor-schedule'),
     path('room/', views.doctor_room, name='doctor-room'),
     # path('about/', views.about, name='library-about'),
