2b5a6f30c4c53ee4ee384d533109c45b8b3f9da4
gretaivan <greta.ivan91@gmail.com>
2021-05-19 15:55:32 +0100

    adds async chat

--- mediquick/mediquick/settings.py
+++ mediquick/mediquick/settings.py
@@ -155,22 +155,9 @@ ASGI_APPLICATION = 'mediquick.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        ### Method 1: Via redis lab
-        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        # 'CONFIG': {
-        #     "hosts": [
-        #       'redis://h:<password>;@<redis Endpoint>:<port>' 
-        #     ],
-        # },
-
-        ### Method 2: Via local Redis
-        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        # 'CONFIG': {
-        #      "hosts": [('127.0.0.1', 6379)],
-        # },
-
-        ### Method 3: Via In-memory channel layer
-        ## Using this method.
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
