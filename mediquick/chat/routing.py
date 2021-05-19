c270407ae9bc80de80bf375de100cadf1e3afb71
gretaivan <greta.ivan91@gmail.com>
2021-05-19 16:46:15 +0100

    adds templates routing and rest api for chat

--- mediquick/chat/routing.py
+++ mediquick/chat/routing.py
@@ -1,7 +1,14 @@
-from django.urls import re_path
+# from django.urls import re_path
 
-from . import consumers
+# from . import consumers
+
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+# ]
+from chat import consumers
+
+from django.conf.urls import url
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    url(r'^ws$', consumers.ChatConsumer),
 ]
