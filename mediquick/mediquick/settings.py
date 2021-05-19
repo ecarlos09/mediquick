f126691b2f5b872f705c869e11baba83c2018bb1
Elwin <elwin.carlos09@gmail.com>
2021-05-19 12:47:59 +0100

    configure database deploymanet settings

--- mediquick/mediquick/settings.py
+++ mediquick/mediquick/settings.py
@@ -185,4 +185,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
