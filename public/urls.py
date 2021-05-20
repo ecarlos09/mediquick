9f3f1dab5476dd6d9784e53525d688d36410f699
RKuppan97 <ryan_kuppan@hotmail.com>
2021-05-20 17:17:38 +0100

    added about page and routing etc

--- public/urls.py
+++ public/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('forbidden/', views.forbidden_405, name='405'),
     path('not-found/', views.not_found_404, name='404'),
     path('server-error/', views.server_error_500, name='500'),
+    path('policy/', views.policy, name='policy'),
+    path('support/', views.support, name='support'),
+    path('testimonials/', views.support, name='testimonials'),
 ]
