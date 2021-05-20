9bee5adde1a983c79bdd139086d1a66b84db61e0
Elwin <elwin.carlos09@gmail.com>
2021-05-20 08:07:04 +0100

    add tests for 404 and 500

--- public/tests.py
+++ public/tests.py
@@ -18,4 +18,20 @@ class TestBasicViews(TestCase):
         response = self.c.get(reverse('405'))
         expected_html = render_to_string('public/errors/405.html')
         assert "Oops" in expected_html
+        assert "not allowed" in expected_html
         assert "public/errors/405.html" in [t.name for t in response.templates]
+
+    def test_not_found_404(self):
+        response = self.c.get(reverse('404'))
+        expected_html = render_to_string('public/errors/404.html')
+        assert "Oops" in expected_html
+        assert "not be found" in expected_html
+        assert "public/errors/404.html" in [t.name for t in response.templates]
+
+    def test_server_error_500(self):
+        response = self.c.get(reverse('500'))
+        expected_html = render_to_string('public/errors/500.html')
+        assert "Oops" in expected_html
+        assert "not you" in expected_html
+        assert "it's us" in expected_html
+        assert "public/errors/500.html" in [t.name for t in response.templates]
