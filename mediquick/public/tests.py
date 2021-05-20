be9081c73b8e73c7b5517cf36f80019bf03f2f1f
Elwin <elwin.carlos09@gmail.com>
2021-05-20 12:05:28 +0100

    reinstall coverage and add tests for policy about and support

--- mediquick/public/tests.py
+++ mediquick/public/tests.py
@@ -30,8 +30,27 @@ class TestBasicViews(TestCase):
 
     def test_server_error_500(self):
         response = self.c.get(reverse('500'))
-        expected_html = render_to_string('public/errors/500.html')
+        expected_html = render_to_string('publicpolicy.html')
         assert "Oops" in expected_html
         assert "not you" in expected_html
         assert "it's us" in expected_html
         assert "public/errors/500.html" in [t.name for t in response.templates]
+
+    def test_policy(self):
+        response = self.c.get(reverse('policy'))
+        expected_html = render_to_string('public/policy.html')
+        assert "Privacy Policy" in expected_html
+        assert "mediquick.adm1n@outlook.com" in expected_html
+        assert "public/policy.html" in [t.name for t in response.templates]
+
+    def test_about(self):
+        response = self.c.get(reverse('about'))
+        expected_html = render_to_string('public/about.html')
+        assert "Our aim" in expected_html
+        assert "public/about.html" in [t.name for t in response.templates]
+
+    def test_support(self):
+        response = self.c.get(reverse('support'))
+        expected_html = render_to_string('public/support.html')
+        assert "mediquick.adm1n@outlook.com" in expected_html
+        assert "public/support.html" in [t.name for t in response.templates]
