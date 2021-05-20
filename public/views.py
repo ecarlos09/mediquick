842e824cc4339a5cb73e22f9132ce953206b2c8c
Elwin <elwin.carlos09@gmail.com>
2021-05-20 08:06:52 +0100

    add tests for 404 and 500

--- public/views.py
+++ public/views.py
@@ -8,6 +8,11 @@ def index(request):
 def forbidden_405(request):
     return render(request, 'public/errors/405.html')
 
+def not_found_404(request):
+    return render(request, 'public/errors/404.html')
+
+def server_error_500(request):
+    return render(request, 'public/errors/500.html')
     
 # def login(request):
 #     return render(request, 'public/login.html', title="login")
