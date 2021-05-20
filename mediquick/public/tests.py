c967434902ae9f8c25e9e0280491b0c956192771
Elwin <elwin.carlos09@gmail.com>
2021-05-20 07:34:38 +0100

    add tests for public views

--- mediquick/public/tests.py
+++ mediquick/public/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your tests here.
+
+class TestBasicViews(TestCase):
+    c = Client()
+
+    def test_index(self):
+        response = self.c.get(reverse('index'))
+        expected_html = render_to_string('public/home.html')
+        assert "MediQuick" in expected_html
+        assert "public/home.html" in [t.name for t in response.templates]
+
+    def test_forbidden_405(self):
+        response = self.c.get(reverse('405'))
+        expected_html = render_to_string('public/errors/405.html')
+        assert "Oops" in expected_html
+        assert "public/errors/405.html" in [t.name for t in response.templates]
