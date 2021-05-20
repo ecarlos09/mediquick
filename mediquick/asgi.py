16efa73dbd2ab5b46aea6f93a88b387eaf2acbe3
Elwin <elwin.carlos09@gmail.com>
2021-05-20 18:51:37 +0100

    configure for redis deployment

--- mediquick/asgi.py
+++ mediquick/asgi.py
@@ -7,35 +7,38 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-# import os
-# # import channels
+import os
+import django
+from django.core.asgi import get_asgi_application
 
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-# import chat.routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediquick.settings')
+django.setup()
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediquick.settings')
+# import channels
 
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-
-# # application = get_asgi_application()
-  
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
     ),
 })
+
+# application = get_asgi_application()
+  
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import chat.routing
+
+# application = ProtocolTypeRouter({
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
